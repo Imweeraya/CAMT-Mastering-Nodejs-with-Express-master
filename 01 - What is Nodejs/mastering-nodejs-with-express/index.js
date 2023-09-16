dda32e4e7fa8942bc4a24a461fff9c4601c8c402
Imweeraya <weerayakosaldilok@gmail.com>
2023-09-16 11:54:44 +0700

    Lab 01

---
+++ 01 - What is Nodejs/mastering-nodejs-with-express/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Node.js");
+console.log(global);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(exports);
+console.log(module.exports === exports);
+
+console.log("<<<< Blocking >>>>");
+const fs = require("fs");
+const content = fs.readFileSync("./data.js", "utf8");
+console.log(content);
+
+console.log("<<<< Non Blocking >>>>");
+fs.readFile("./data.js", "utf8", function (err, content) {
+  console.log(content);
+});
